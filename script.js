b2c37b5ba93888a4a9219212fcf9fea5637d5151
Vandana5s <98863444+Vandana5s@users.noreply.github.com>
2023-07-09 13:49:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+//TMDB
+const API_KEY = "api_key=230c0ed9cb36d2ad91f48634ea487a4c";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// add these features later
+// const tagsEl = document.getElementById("tags");
+// const prev = document.getElementById("prev");
+// const next = document.getElementById("next");
+// const current = document.getElementById("current");
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  lastUrl = url;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results);
+      showMovies(data.results);
+    });
+}
+
+function showMovies(data) {
+  main.innerHTML = "";
+  data.forEach((movie) => {
+    const { title, poster_path, vote_average, overview, id } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = ` <img
+    src="${IMG_URL + poster_path}"
+    alt="${title}"
+  />
+  <div class="movie-info">
+  <h3>${title}</h3>
+    <span class="${getColor(vote_average)}">${vote_average}</span>
+  </div>
+  <div class="overview">
+   ${overview};
+  </div>
+    
+    
+    `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getColor(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(searchURL + "&query=" + searchTerm);
+  } else {
+    getMovies(API_URL);
+  }
+});
